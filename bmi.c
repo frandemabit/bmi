18978340f1fed0f2622e0a4643b4c229f95d1fa8
frandemabit <franciscodemariabullano@gmail.com>
2026-06-07 18:22:11 -0300

    Add files via upload

---
+++ bmi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    float peso, altura, imc;
+
+    printf("Ingrese el peso en kg: ");
+    scanf("%f", &peso);
+
+    printf("Ingrese la altura en metros: ");
+    scanf("%f", &altura);
+
+    imc = peso / (altura * altura);
+
+    printf("\nSu indice de masa corporal es: %.2f\n\n", imc);
+
+    printf("    Indice    |  Condicion\n");
+    printf("-----------------------------\n");
+    printf("    <18.5     |  Bajo peso\n");
+    printf("18.5 a 24.9   |  Normal\n");
+    printf("25.0 a 29.9   |  Sobrepeso\n");
+    printf("    >=30      |  Obesidad\n");
+
+    return 0;
+}
